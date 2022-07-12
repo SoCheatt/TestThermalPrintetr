ac8b14d10a8cab30e0bbacf78b4bb3a3d8826389
SoCheatt <momsocheat60@gmail.com>
2022-07-12 09:35:38 +0700

    first commit

---
+++ ThermalPrinterTest/PrintListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PrintListViewController.swift
+//  ThermalPrinterTest
+//
+//  Created by Mom Socheat on 06/07/2022.
+//
+
+import Foundation
---
+++ ThermalPrinterTest/View/Cell/InvoiceHeaderCell.swift
@@ -0,0 +1,23 @@
+//
+//  InvoiceHeaderCell.swift
+//  ThermalPrinterTest
+//
+//  Created by Mom Socheat on 06/07/2022.
+//
+
+import UIKit
+
+class InvoiceHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
